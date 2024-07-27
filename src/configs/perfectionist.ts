f79191393eaa04a5c51ba92d4333aee5fa09af3a
Li (Boris) Ding <ding@pleisto.com>
2024-07-27 15:50:53 +0800

    chore: update deps

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -1,7 +1,6 @@
-import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural'
+import pluginPerfectionist from 'eslint-plugin-perfectionist'
 
 import type { TypedFlatConfigItem } from '../types'
-
 
 /**
  * Optional perfectionist plugin for props and items sorting.
@@ -12,7 +11,61 @@ export async function perfectionist(): Promise<TypedFlatConfigItem[]> {
   return [
     {
       name: 'pleisto/perfectionist/setup',
-      ...perfectionistNatural
+      plugins: {
+        perfectionist: pluginPerfectionist
+      },
+      rules: {
+        'perfectionist/sort-intersection-types': [
+          'error',
+          {
+            type: 'natural',
+            order: 'asc'
+          }
+        ],
+        'perfectionist/sort-named-imports': [
+          'error',
+          {
+            type: 'natural',
+            order: 'asc'
+          }
+        ],
+        'perfectionist/sort-named-exports': [
+          'error',
+          {
+            type: 'natural',
+            order: 'asc'
+          }
+        ],
+        'perfectionist/sort-object-types': [
+          'error',
+          {
+            type: 'natural',
+            order: 'asc'
+          }
+        ],
+        'perfectionist/sort-jsx-props': [
+          'error',
+          {
+            type: 'natural',
+            order: 'asc'
+          }
+        ],
+        'perfectionist/sort-imports': [
+          'error',
+          {
+            type: 'natural',
+            order: 'asc'
+          }
+        ],
+        'perfectionist/sort-exports': [
+          'error',
+          {
+            type: 'natural',
+            order: 'asc'
+          }
+        ],
+        'perfectionist/sort-objects': 'off'
+      }
     }
   ]
 }
